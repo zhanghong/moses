e51de13c5e3cf82c59e03bf0900658dc9751de79
zhanghongbean <zhanghong_bean@126.com>
2019-12-17 17:38:05 +0800

    基础布局

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,12 @@
 <?php
+
+/**
+ * 页面根结点类名
+ * @Author   zhanghong(Laifuzi)
+ * @DateTime 2019-12-10
+ * @return   [type]             [description]
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
