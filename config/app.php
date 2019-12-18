2beed6d07d24d6eb469478ba02601857699ac701
zhanghongbean <zhanghong_bean@126.com>
2019-12-18 17:10:01 +0800

    短信调试

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 发送短信
+        App\Providers\EasySmsServiceProvider::class,
     ],
 
     /*
