bd0d3ad13421e904d72db825220560a2cb0ece3a
zhanghongbean <zhanghong_bean@126.com>
2019-12-18 17:42:34 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Http\Request;
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', 'Core\VerificationCodesController@store')
+Route::prefix('v1')
+->namespace('Api')
+->name('api.v1.')
+->group(function () {
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))
+    ->group(function () {
+        // 短信验证码
+        Route::post('verificationCodes', 'Core\VerificationCodesController@store')
         ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', 'Core\UsersController@store')
+        // 用户注册
+        Route::post('users', 'Core\UsersController@store')
         ->name('users.store');
+    });
 });
