227797c99de3944d893e2c5e43a5e6280d026e66
zhanghongbean <zhanghong_bean@126.com>
2019-12-20 17:35:52 +0800

    语法错误

--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' = '',
+            'prefix' => '',
             // 'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
